c6666170eabb8370668766c0c13e87358771bf37
Zoherifitia <99518636+Zoherifitia@users.noreply.github.com>
2022-03-18 17:39:58 +0100

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,6 +1,6 @@
-let mot="ABCnBA"
 
-//premier methode
+
+//PREMIERE METHODE
 function p1(mot){
     let tab1=[]
     let tab2=[]
@@ -13,13 +13,33 @@ function p1(mot){
     if(tab1.length==tab2.length){
         for(let i=0;i<tab2.length;i++){
             if(tab1[i]!=tab2[i])
-               return("ce mot n'est pas palindrome")
-            
+               return "ce mot n'est pas palindrome"
         }
-        return("ce mot est palindrome")
+        return "ce mot est palindrome"  
+    }
+}
+
+
+//DEUXIEME METHODE
+function p1(mot){
+    for(let i=0;i<mot.length;i++){
+        if(mot[i]!=mot[mot.length-1-i])
+            return 'pas palindrome'
         
     }
-        
-
+    return 'palindrome'
 }
-console.log(p1(mot))
+
+
+//TROISIEMME METHODE
+function p2(mot){
+    
+    if(mot.length==1)
+        console.log 'palindrome'
+    else if(mot.length>1){
+        for(let i=0;i<mot.length;i++){
+            if(mot[i]==mot[mot.length-1])
+                p2(mot.slice(i+1,mot.length-1-i))
+        }
+    }
+}
